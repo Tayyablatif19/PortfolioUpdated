13a5d76b0aa65f1137839112823950ba28c0d61e
Tayyab Latif <mlatif.bee21seecs@seecs.edu.pk>
2026-04-01 19:26:29 +0500

    minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  const repoName = 'portfolio';
+  const isProd = mode === 'production';
+
   return {
+    base: isProd ? `/${repoName}/` : '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
